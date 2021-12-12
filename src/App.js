8a0ed5731a52f7722b49b0a84dec0ece05839477
Vansh Sharma <vanshsharma9354@gmail.com>
2021-12-12 15:05:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React from "react";
+import Die from "./Die";
+import Confetti from "./Confetti";
+import { nanoid } from "nanoid";
+
+export default function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+  const [tenzies, setTenzies] = React.useState(false);
+  const [timesDieRolled, setTimesDieRolled] = React.useState(0);
+
+  React.useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((die) => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+    }
+  }, [dice]);
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    };
+  }
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie());
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    if (!tenzies) {
+      setTimesDieRolled((oldValue) => oldValue + 1);
+      setDice((oldDice) =>
+        oldDice.map((die) => {
+          return die.isHeld ? die : generateNewDie();
+        })
+      );
+    } else {
+      setTenzies(false);
+      setTimesDieRolled(0);
+      setDice(allNewDice());
+    }
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      noOfTimes={die.noOfTimes}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
+
+  return (
+    <div className="center">
+      {" "}
+      <main>
+        {tenzies && <Confetti />}
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls. Player with{" "}
+          <b>least number of times Die Rolled wins the game.</b>
+        </p>
+        <div className="dice-container">{diceElements}</div>
+        <button className="roll-dice" onClick={rollDice}>
+          {tenzies ? "New Game" : `Roll`}
+        </button>
+        <h1 className="dieRolled">Die Rolled: {timesDieRolled}</h1>
+      </main>
+    </div>
+  );
+}
---
+++ src/README.md
@@ -0,0 +1,24 @@
+<h1 align="center">Tenzies</h1>
+
+---
+
+#### How to PLAY:
+
+- Select the number you want to hold
+- Roll until all dice are the same.
+- Click each die to freeze it at its current value between rolls.
+- Player with least number of times Die Rolled wins the game.
+  > You can also change the number in between the game.
+
+---
+
+#### Tech Stack
+
+- React
+- `React Confetti` library to show confetti at the end.
+- `Nanoid` to generate unique ids.
+
+***
+
+### Connect
+[Twitter](https://twitter.com/Vanshsh2701)
